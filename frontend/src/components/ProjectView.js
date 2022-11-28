a88a2b91c8263f2423c01bed0d35c73825fbc3c0
jash-05 <jash.sheth@gmail.com>
2022-11-28 03:14:37 -0800

    added API calls for project page

--- frontend/src/components/ProjectView.js
+++ frontend/src/components/ProjectView.js
@@ -1,9 +1,49 @@
-import React from "react";
-import {Sidebar, Menu, MenuItem, SubMenu, MenuItemFR} from "react-pro-sidebar";
-import {Button, Container, Row, Col} from "react-bootstrap";
+import React, {useEffect, useState} from "react";
+import {Container, Row, Col} from "react-bootstrap";
+import {ENDPOINT_MAPPINGS} from "../utils/config";
+import {makeGetRequest} from "../utils/makeRequest";
 import Editor from "./Editor";
+import {BsFillPeopleFill} from "react-icons/bs";
+import Dropdown from "react-bootstrap/Dropdown";
 
-const ProjectView = () => {
+const ProjectView = ({projectId}) => {
+	const [projectName, setProjectName] = useState("");
+	const [description, setDescription] = useState("");
+	const [owner, setOwner] = useState([]);
+	const [pages, setPages] = useState([]);
+	const [collaborators, setCollaborators] = useState([]);
+
+	const getProjectDetails = async () => {
+		const project = await makeGetRequest(
+			ENDPOINT_MAPPINGS.getProjectDetails,
+			[projectId]
+		);
+		if (project) {
+			setProjectName(project.name);
+			setDescription(project.description);
+			setOwner([project["ownerId"]["email"]]);
+			setPages(project.pages);
+		}
+	};
+
+	const getCollaborators = async () => {
+		const project = await makeGetRequest(
+			ENDPOINT_MAPPINGS.viewCollaborators,
+			[projectId]
+		);
+		if (project) {
+			setCollaborators(
+				project.collaborators.map(
+					(collaboratorObj) => collaboratorObj["userId"]["email"]
+				)
+			);
+		}
+	};
+	useEffect(() => {
+		getProjectDetails();
+		getCollaborators();
+	}, []);
+	console.log("COLL");
 	return (
 		<Container fluid>
 			<Row>
@@ -11,7 +51,34 @@ const ProjectView = () => {
 					xs={2}
 					className="border-end border-dark border-2 pt-5"
 					style={{height: "100%"}}>
-					<div style={{height: "90vh"}}>
+					<div style={{height: "85vh"}}>
+						<Row className="px-1 border-bottom border-2 mb-3">
+							<p className="text-center display-6">
+								{projectName}
+							</p>
+							<p className="text-center fs-6">{description}</p>
+							<p className="text-center fs-6">
+								<Dropdown>
+									<Dropdown.Toggle
+										variant=""
+										id="dropdown-basic">
+										<BsFillPeopleFill /> View Collaborators
+									</Dropdown.Toggle>
+
+									<Dropdown.Menu>
+										{[...owner, ...collaborators]?.map(
+											(collaborator) => {
+												return (
+													<Dropdown.Item>
+														{collaborator}
+													</Dropdown.Item>
+												);
+											}
+										)}
+									</Dropdown.Menu>
+								</Dropdown>
+							</p>
+						</Row>
 						<Row className="my-2 px-2">
 							<p className="text-center fs-6">Master Project</p>
 						</Row>
