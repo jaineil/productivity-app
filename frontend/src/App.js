ab5197cafacb816054ff70f59088dbe80764525e
jash-05 <jash.sheth@gmail.com>
2022-11-30 18:30:27 -0800

    fix toast

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import ProjectView from "./components/ProjectView";
 import Landing from "./components/Landing";
 import Navbar from "./components/Navbar";
 import {Routes, Route, useLocation} from "react-router-dom";
+import {Toaster} from "react-hot-toast";
 
 function App() {
 	const location = useLocation();
@@ -15,6 +16,7 @@ function App() {
 				<Route path="/project/:id" element={<ProjectView />} />
 				<Route index path="/" element={<Landing />} />
 			</Routes>
+			<Toaster />
 		</div>
 	);
 }
